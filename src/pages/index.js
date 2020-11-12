b4cc23bc588e33c705d2d67a56f12f4453028331
Rajiv Singh <67971676+prudentcares@users.noreply.github.com>
2020-11-12 17:24:46 +0000

    update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Thanks for visiting us.</p>
+    <p>We will be launching soon!.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
